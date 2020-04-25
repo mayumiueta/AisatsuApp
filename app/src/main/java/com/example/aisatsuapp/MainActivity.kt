00adc3c44533b4874131daa8b055b1755eafe7fc
mayumiueta <mueta@koto.kpu-m.ac.jp>
2020-04-26 06:55:12 +0900

    timePickerDialog

--- app/src/main/java/com/example/aisatsuapp/MainActivity.kt
+++ app/src/main/java/com/example/aisatsuapp/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.aisatsuapp
 
+import android.app.TimePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() , View.OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+        {
+            override fun onClick(v: View) {
+                if (v.id == R.id.button1) {
+                    showTimePickerDialog()
+                }
+            }
+        }
+
+        private fun showTimePickerDailog() {
+            val timePickerDialog = TimePickerDialog(
+                this,
+            TimePickerDialog.OnTimeSetListener{ view, hour, minute ->
+                Log.d("UI_PARTS", "$hour:$minute")
+
+            },
+       13, 0, true)
+            timePickerDialog.show()
+            }
     }
+}
+
 }
